5a08bf556f4932d0acd16e96478218dd49e07a69
Phil Schalm <pnomolos@gmail.com>
2012-03-29 08:42:21 -0700

    Updating licence strings.

--- models/Model.php
+++ models/Model.php
@@ -2,7 +2,7 @@
 /**
  * Lithium: the most rad php framework
  *
- * @copyright	  Copyright 2012, Mariano Iglesias (http://marianoiglesias.com.ar)
+ * @copyright	  Copyright 2012, Phil Schalm (http://github.com/pnomolos)
  * @license		  http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
